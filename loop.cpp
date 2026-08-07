b2da92bbcb9bf3c51862bc7f541ac0781f9a59f9
vansh sirohi <vansh.25b01011065@abes.ac.in>
2026-08-07 07:52:19 +0000

    todays task

---
+++ loop.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"enter a number";
+//     cin>>a;
+//     for(int i=0;i<=10;i++){
+//         cout<<"hello"<<endl;
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main( ){
+    for(int i=0;i<=5;i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
