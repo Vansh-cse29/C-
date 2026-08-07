a5983527989beb8f2fcfecca1fd54f24d35cf7fc
vansh sirohi <vansh.25b01011065@abes.ac.in>
2026-08-07 07:07:53 +0000

    your commit message

---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main( ){
+    int a;
+     cout<<"enter a number";
+    cin>>a; 
+
+    if (a>10){
+    cout<<"accepted\n";
+    }
+    else{
+    cout<<"not accepted\n";
+}
+return 0;
+}
---
+++ result.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter your marks";
+    cin>>a;
+    if (a<33){
+        cout<<"fail"<<endl;
+    }
+    else{
+        cout<<"pass"<<endl;
+    }
+    return 0;
+
+}
